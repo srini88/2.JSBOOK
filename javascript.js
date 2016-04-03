3af9519376849afe1b4f07a53b4396d932d928c1
srini88 <pvsrinivas.ece@gmail.com>
2016-04-03 16:10:30 -0500

    isArray

--- javascript.js
+++ javascript.js
@@ -1,44 +1,24 @@
-// identifying reference types 
-
-// typeof on a function returns function 
 
 
-var obj = {};
+var arr = [2, 3, 4];
+
+console.log(arr instanceof Array);  // true
+console.log(Array.isArray(arr));  // true
+
+//isArray is a method on Array 
 
 
-console.log(typeof obj);  //object 
+console.log(Array.isArray([]));  // true
+console.log(Array.isArray([1]));  // true
+console.log(Array.isArray(new Array()));  //true
 
-var arr = [];
+// below all false 
 
-console.log(typeof arr);  //object - array is kindof object 
-
-
-function srini(){
-
-	return 1;
-}
-
-console.log(typeof srini);   //function - remmeber no paranthesis for fn
-
-
-// for all reference types other than functions, typeof returns object 
-// istenad use instanceof 
-
-
-// instanceof takes two - one before and one after - before is the name of the object you wanna check - after is the constructor name like Object , array etc 
-
-
-console.log( srini instanceof Function);    // true
-console.log( srini instanceof Object);    // true  , not sure why it returns true 
-
-console.log(obj instanceof Object);  // true
-
-
-console.log(arr instanceof Object);  // true  - everything is an instance of object I guess 
-
-// The instanceof operator can identify inherited types. That means
-// every object is actually an instance of Object because every reference type
-// inherits from Object.
-
-
-console.log(arr instanceof Array);   // true
+Array.isArray();
+Array.isArray({});
+Array.isArray(null);
+Array.isArray(undefined);
+Array.isArray(17);
+Array.isArray('Array');
+Array.isArray(true);
+Array.isArray(false);
