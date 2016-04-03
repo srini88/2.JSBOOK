e7af1058ed77c20e61586bc4aefedec12e2163f3
srini88 <pvsrinivas.ece@gmail.com>
2016-04-03 17:38:50 -0500

    sort

--- javascript.js
+++ javascript.js
@@ -1,95 +1,123 @@
+// functions as values 
 
-function srini(){
+// Because JavaScript has first-class functions, you can use them just as
+// you do any other objects. You can assign them to variables, add them
+// to objects, pass them to other functions as arguments, and return them
+// from functions. Basically, you can use a function anywhere you would
+// use any other reference value. This makes JavaScript functions incredibly
+// powerful. 
 
+
+
+
+// function sayHi(){
+// 	console.log("Hi!");
+// }
+
+
+// sayHi();  //Hi
+
+
+// var sayHi2 = sayHi;  // assigning function to a variable
+
+// sayHi2();  //Hi
+
+// both sayHi and sayHi2 are pointing to the same function means either can be executed with teh same result 
+
+
+// functions are objects 
+
+// you can pass a fn into another fn as an argument 
+
+
+// sort() - accepts a comparison method as an optional parameter 
+// this fn comparison is called whenever 2 values in the array must be compared 
+
+
+
+// var arr = [3, 2, 1, 6, 10, 8];
+
+// console.log(arr); //[3, 2, 1, 6, 10, 8]
+
+// // in the sort - not using a comparison function 
+// // in that case sort() converts every item to a string and then performs a comparison 
+// // you cannot accrately sort an array of numbers without sepcifying a comparison function 
+
+
+// var arr1 = arr.sort(); // also don't use arr1 just arr.sort(); does sorting inline 
+
+// console.log(arr1); //[1, 10, 2, 3, 6, 8]
+// console.log(arr); //[1, 10, 2, 3, 6, 8]  // does it inline too 
+
+
+// now correct way 
+
+var array1 = [3, 2, 1, 6, 10, 8];
+
+function compare (num1, num2){
+	return num1-num2;
 }
 
-console.log(typeof srini);  // function
-console.log(srini instanceof Function);  // true
+//array1.sort(compare()); // wrong syntax - getting wrong results // dont use ()
 
 
-
-// chapter 2 - function s
-
-// The defining characteristic of a function—what distinguishes
-// it from any other object—is the presence of an internal property named [[Call]].
+array1.sort(compare);
+console.log(array1);  //[1, 2, 3, 6, 8, 10]
 
 
-// functions have internal property called [[call]]
-// call is unique to the fucntion and indicated that the object can be executed 
-
-// because of this typeof (as only functions have this property) returns function for any object with [[call]] property 
-
-// Internal properties are not accessible via code but rather
-// define the behavior of code as it executes.
+//aother way of writing 
 
 
-// ECMAScript defines multiple internal properties for objects in JavaScript, and these internal properties
-// are indicated by double-square-bracket notation
+var array2 = [3, 2, 1, 6, 10, 8];
 
-// functions are kind of objects in JS - behavior is central to JS 
-
-
-// Declarations vs Expressions
-
-
-// function declaration begins with function keyword and immediately includes name of the fun
-
-function add (num1, num2){
-	return num1+num2;
-}
-
-// 2nd form function expression - does not need a name after function 
-// these functions are considered anonymous as the function object itself has no name 
-
-var add = function(num1, num2) {
-return num1 + num2;
+var comp2 = function (num1, num2){
+	return num1-num2;
 };
 
-// end with semicolon // no function name 
-
-// differences - when using declarations you can just call the function before it is declared 
-
-var result = add(5, 5);
-function add(num1, num2) {
-return num1 + num2;
-}
 
 
-// the above code will work 
+array2.sort(comp2);
+console.log(array2);  //[1, 2, 3, 6, 8, 10]
 
-// Function declarations are hoisted to the top of the context (either
-// the function in which the declaration occurs or the global scope) when the // code is executed. That means you can actually define a function after it is // used in code without generating an error
-
-// since the function name is known ahead of the time function hoisting happens only for function declarations 
-
-// fucntion expressions cannot be hoisted because the functions can be referenced only through a variable 
+//the default comparison converts all
+//values to strings before comparing them.
 
 
-// var result  = add1 (5, 5);
+// The sort() method sorts the items of an array.
 
-// var add1 = function (num1, num2){
-// 	return num1+num2;
-// };
+// The sort order can be either alphabetic or numeric, and either ascending (up) or descending (down).
 
+// By default, the sort() method sorts the values as strings in alphabetical and ascending order.
 
-// console.log(result);  // will not work - as add1 is above the definition
-// error uncaught TypeError 
-// cannot happen because the function can be referenced only through a variable 
+// This works well for strings ("Apple" comes before "Banana"). However, if numbers are sorted as strings, "25" is bigger than "100", because "2" is bigger than "1".
 
-var add2 = function (num1, num2){
-	return num1+num2;
-};
+// Because of this, the sort() method will produce an incorrect result when sorting numbers.
 
-var result2  = add2 (5, 5);
+// You can fix this by providing a "compare function" (See "Parameter Values" below).
+
+// Note: This method changes the original array.
 
 
+var arr3 = ["apple", "choco", "banana", "banyan"];
 
-console.log(result2);  // // This is working, as add2 call is below the definition 
+arr3.sort();
+
+console.log(arr3);  // correct ["apple", "banana", "banyan", "choco"] in case of strings 
 
 
-// this is the main difference between fun declaration and fun expression , how the hoisting takes place 
+// when using compare function on an array of strings, I'm getting wrong results - so dont use compare for strings, only for integers 
 
-// As long as you always define functions before using them, you can use
-// either function declarations or function expressions.
+var arr4 = ["apple", "choco", "banana", "banyan"];
+
+arr4.sort(function (first, second){
+	return first-second;
+});
+
+console.log(arr4); //["apple", "choco", "banana", "banyan"]
+
+
+
+// for descending order use return b-a
+
 
 
--- notes.js
+++ notes.js
@@ -190,3 +190,13 @@ objects of any other reference type.
 
 
 
+In computer science, a programming language is said to support first-class functions (or function literal) if it treats functions as first-class objects. Specifically, this means that the language supports constructing new functions during the execution of a program, storing them in data structures, passing them as arguments to other functions, and returning them as the values of other functions.
+This page also illustrates it beautifully:
+
+Really, just like any other variable
+A function is an instance of the Object type
+A function can have properties and has a link back to its constructor method
+You can store the function in a variable
+You can pass the function as a parameter to another function
+You can return the function from a function
+
