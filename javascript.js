f17f19a598504ec10fd8269d6f8e9ce966009771
srini88 <pvsrinivas.ece@gmail.com>
2016-04-02 22:50:46 -0500

    arr bracket and dot notation

--- javascript.js
+++ javascript.js
@@ -1,38 +1,54 @@
 
 
-// You almost always define functions using their literal form. In fact, using
-// the Function constructor is typically discouraged given the challenges of
-// maintaining, reading, and debugging a string of code rather than actual
-// code, so you’ll rarely see it in code.
+// bracket notation and dot notation on the properties 
 
 
-// functions always in a literal form 
+// dot notation
+var arr  = [];
+
+console.log(arr);
 
 
-// object literal way for functions 
+arr.push(123);
 
-function reflect(value){
-	return value;
-}
+console.log(arr);  //[123]
 
 
-console.log(reflect(5));  //5
+// bracket notation
 
-// 2nd way using the new and constructor function 
-// use the function constructor 
-// you use the function name in the first
-var reflect1 = new Function("value", "return value");
+// With bracket notation, the name of the method is now included in a
+// string enclosed by square brackets
+
+// var arr1 = [];
+
+// arr1["push"](123);
+
+// console.log(arr1);  //[123]
 
 
-console.log(reflect1(5));  //5
+// var arr1 = [];
+
+// arr1["push"](123);
+// arr1["push"](456);  // adds another - does not override - remember
 
 
-// Regular Expression Literals
-// JavaScript also has regular expression literals that allow you to define regular
-// expressions without using the RegExp constructor. Regular expression
-// literals look very similar to regular expressions in Perl: The pattern is contained
-// between two slashes, and any additional options are single characters
-// following the second slash. For example:
-// var numbers = /\d+/g;
-// // is the same as
-// var numbers = new RegExp("\\d+", "g");
+
+// console.log(arr1);  //[123, 456]
+
+
+
+
+var arr1 = [];
+
+var temp = "push";
+
+
+arr1[temp](98);
+console.log(arr1);  //98
+
+// why the hell you use bracket notation ?
+
+// This syntax is very useful when you want to dynamically decide which
+// property to access.
+
+// temp has a value of push - so push is called on the array 
