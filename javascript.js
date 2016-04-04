96f95f42c93a598d94ca8ff2e0e71b9c52945218
srini88 <pvsrinivas.ece@gmail.com>
2016-04-03 21:45:01 -0500

    object methods a

--- javascript.js
+++ javascript.js
@@ -1,53 +1,58 @@
+// object methods 
+// you can add and remove properties from objecta at any time 
+
+// when a property value is a function, the property is considered a method 
+
+var person = {
+
+	name :"srini",
+	sayName : function(){
+		console.log(person.name);   // using person.name instead of this.name
+		// using person.name creates a tight coupling between the method and the object 
+		// not good. 
+		// if you change the variable name, you also need to change the reference to that name in the method 
+		// hard to use the same function for different objects 
 
 
-// function overloading in javascript 
-
-// most oo languages support function overloading, ability of a single function to have multiple signatures 
-// function signature made of function name + type of parameters the function expects 
-// one function with 1 arg, same function again with 2 args 
-// the language determines which version of a function to call based on the arguments that are passed in 
-
-// As mentioned previously, JavaScript functions can accept any number
-// of parameters, and the types of parameters a function takes aren’t specified
-// at all. That means JavaScript functions don’t actually have signatures.
-// A lack of function signatures also means a lack of function overloading
-
-
-function sayHello(){
-	console.log("Hello");
-}
-
-function sayHello(message){
-	console.log(message);
-}
-
-
-sayHello("BOO");  // prints BOO
-
-// In JavaScript, however, when you define multiple functions
-// with the same name, the one that appears last in your code wins.
-
-// The earlier function declarations are completely removed, and the last
-// is the one that is used
-
-// you still can mimic function overloading in JS
-
-
-function sayHi(){
-
-	if (arguments.length ==0){
-		console.log("No args passed");
-	}
-	else
-	{
-		console.log("Args passed");
 	}
 
-}
 
-sayHi();  // no args passed 
-sayHi("HE");  // Args passed
+};
 
-// In practice, checking the named parameter against undefined is more common
-// than
-// relying on arguments.length.
+person.sayName();  // Srini
+person.sayName("boo");  // still Srini  // boo is getting discarded in the function
+// to get around using person.name above 
+// every scope in JS has a this object that represents the calling object for the function 
+
+// in the global scope, this represents the global object (window in web browsers)
+
+// when a function is called while attached to an object, the value of "this" is equal to that object by default 
+
+// rewriting code with thsi 
+var person1 = {
+
+	name :"srinivas",
+	sayName : function(){
+		console.log(this.name);   // using this.name.
+		
+	}, 
+	setName : function (naam){
+		this.name = naam;
+	}
+};
+
+
+person1.sayName();  // srinivas
+
+
+// simply trying out 
+var person4 = person1;   // this does shallow copy, both point to same variable object that points to the same storage location in memory 
+
+person4.sayName();  // srinivas
+
+person4.setName("viswanath");  // chaning the per4 name to viswanath
+// this affects person1 name too as they both point to the samw location
+
+person4.sayName();  // viswanath 
+person1.sayName();  // viswanath 
+
