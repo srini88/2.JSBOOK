8a05f0c642a68e299156e1a727dc4dedbae1fc81
srini88 <pvsrinivas.ece@gmail.com>
2016-04-03 20:56:12 -0500

    arguments to array conv and sort

--- javascript.js
+++ javascript.js
@@ -1,52 +1,16 @@
-/// you want to write a sum method
-// you dont know how many arguments you need 
+// converting arguments object to an array so that we can use array methods like sort or something 
+// now printing after sorting 
+function printArr(){
+	// converting javascript arguments object to an array 
+	var arrConv = Array.prototype.slice.call(arguments); // by heart this
+	// arrConv is an array
 
-/// so you got to use the arguments[] array 
-
-
-// Sometimes, however, using arguments is actually more effective than
-// naming parameters. For instance, suppose you want to create a function
-// that accepts any number of parameters and returns their sum. You can’t
-// use named parameters because you don’t know how many you will need,
-// so in this case, using arguments
-// is the best option.
-
-// using while loop
-
-// The sum() function accepts any number of parameters and adds them
-// together by iterating over the values in arguments with a while loop
-
-
-function sum(){
-	var result = 0;
-	var len  = arguments.length;   // arguments is accessible inside this function itself 
-	var i = 0;
-	while (i < len){
-		result += arguments[i];
-		i++;
-	}
-	return result;
+	// 2nd way of conversion using array literals 
+	// remember no Array and no prototype
+	// you are calling slice and call directly on []
+	var arrConv1 = [].slice.call(arguments);
+	arrConv1.sort();   // this sort wont work if you have 1, 11, 21, and all  , just using it temporarily 
+	console.log(arrConv);  //[3, 4, 8]   // sorted now
+	console.log(arrConv1);  //[3, 4, 8]   // sorted now
 }
-
-console.log(sum(2, 3));  //5
-console.log(sum(2, 3, 4));  //9
-console.log(sum(2, 3, 4, 5));  //14
-
-
-// using for loop and function expression 
-
-var sum1 = function(){
-	var result = 0;
-	var len = arguments.length;
-
-	for (var i =0; i < len; i++)
-	{
-		result += arguments[i];
-	}
-	return result;
-};
-
-console.log(sum1(2, 3));  //5
-console.log(sum1(2, 3, 4));  //9
-console.log(sum1(2, 3, 4, 5));  //14
-console.log(sum1());  //0
+printArr(3, 8, 4);
