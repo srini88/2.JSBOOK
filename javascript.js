b45d4c66de864a894926da956bf1f6ce3a806ffe
srini88 <pvsrinivas.ece@gmail.com>
2016-04-05 15:08:40 -0500

    in and hasOwnProperty)

--- javascript.js
+++ javascript.js
@@ -27,19 +27,63 @@ var obj1 = {
 
 console.log(obj1);  //Object {name: "Srinivas"}
 
-// Objects you
-// create are always wide open for modification unless you specify otherwise
-// (more on that in “Preventing Object Modification” on page 45).
 
-// you got to prevent object modification
+/////////detecting properties
+// because properties can be added any time, better to check if property exists or not. 
 
-// when a property is first added to an object, JS uses internal method called [[put]] on that object. 
-// same like adding key to a hash table 
-// we are creating an own property name above, that indicates that the specific instance of the object owns that property 
-// property stored directly on the instance, and all operations are performed through that object 
+if (obj1.name){
+	console.log("exists");   // prints exists, but not right way to do it
+}
 
-// above is own proerpy , there are two types of properties on an object 
-// own properties
-// prototype proerpties 
 
-// when a new value is assigned to an existing property, [[set]] takes place instead of [[put]]  - since updating here. 
+
+// The if condition evaluates to true if the value is truthy (an
+// object, a nonempty string, a nonzero number, or true) and evaluates to
+// false if the value is falsy (null, undefined, 0, false, NaN, or an empty string).
+
+// Because an object property can contain one of these falsy values, the
+// example code can yield false negatives. For instance, if person1.age is 0,
+// then the if condition will not be met even though the property exists.
+
+////best approach to check is to use the "in " operator 
+///we are basically checking to see if the given key exists in the hash table 
+
+if ("name" in obj1){   // remember to use "" for properties 
+	console.log("exists using in operator");   // true
+}
+
+console.log("title" in obj1);   //false
+ 
+ ////now how will you check if a method exists 
+
+obj1.sayName = function(){
+	console.log("name is  "+  this.name);
+}
+
+console.log(obj1);  // added the function to the object 
+// check the method using the "in " operator 
+
+console.log("sayName" in obj1);  //true
+console.log("sayName1" in obj1);  //false
+
+// in most cases in operator is the best way to determine if the proerpty exists or not 
+// it does not evaluate the value of the property, we dont want to evaluate like checking with the "if " right 
+
+/// now want to check with the "in" operator if it is an own property or a protyotype property 
+
+// in operator checks both
+// use the hasOwnProperty() method, which exists on all objects and returns true only if the given property exists and is an own property 
+
+console.log(obj1.hasOwnProperty("sayName"));  // "" here also   // true
+////why hasOwnProperty is better than in 
+/// ""in does both own and prototype properties 
+
+
+console.log("sayName" in obj1);  // true
+console.log(obj1.hasOwnProperty("sayName"));  // true 
+
+console.log("toString" in obj1);   // true - this is the problem, toString is a prototype property - present on all objects 
+console.log(obj1.hasOwnProperty("toString"));   // false, which is what we want 
+
+
+
