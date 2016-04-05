8111b5dbf1c7ebec0f82b18e2e749fec3364cb18
srini88 <pvsrinivas.ece@gmail.com>
2016-04-05 15:31:20 -0500

    objectkeys only fetches own properites

--- javascript.js
+++ javascript.js
@@ -9,27 +9,35 @@ obj1.sayName = function(){
 obj1.sayHello = function(){
 	console.log("saying hello : name is  "+  this.name);
 }
-//////////ENUMERATIon
-////by default all properties added to an object are enumerable 
-////iterate over them using a for - in loop
 
-//console.log(obj1);
 var property;
 for (property in obj1){
-	console.log("property is : " + property + " and value is : "+ obj1[property]);
+	//console.log("property is : " + property + " and value is : "+ obj1[property]);
 
 }
-///property value is filled with the next enumerable property on the object until all properties have been used
-/// using bracket notation to retrive the value of the object property 
-//obj1[property]
+///// 2nd way to print properties using the method keys on the Object
+//// it returns a goddammn array 
+var properties  = Object.keys(obj1);
+
+//////Object.keys() to retrieve the enumerable properties
+console.log(properties);  /////["name", "sayName", "sayHello"]
+
+//// now lets try to print the values of the keys as well sd
+
+for (var i = 0; i < properties.length ; i++){  // .length on an array
+	console.log("property is : " + properties[i] + " and value is : "+ obj1[properties[i]]);
+}
+// this for loop prints the same godammn thing 
 
 
-//output 
-// property is : name and value is : Srinivas
-// javascript.js:19 property is : sayName and value is : function (){
-// 	console.log("name is  "+  this.name);
-// }
-// javascript.js:19 property is : sayHello and value is : function (){
-// 	console.log("saying hello : name is  "+  this.name);
-// }
+/////obj1[properties[i]]  correct
+//////obj1.properties[i]  --- wrong, needs to be evaluated right
 
+////why two ways to enumerate the proeprties
+/////using for in - it fetches the protype properties as well
+
+/////////Object.keys(obj) wont fetch that shit 
+// There is a difference between the enumerable properties returned in a for-
+// in loop
+// and the ones returned by Object.keys(). The for-in loop also enumerates prototype
+// properties, while Object.keys() returns only own (instance) properties.
