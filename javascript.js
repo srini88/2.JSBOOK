338432713a2bb020150979507876369aaa541de8
srini88 <pvsrinivas.ece@gmail.com>
2016-04-06 16:30:31 -0500

    define properties

--- javascript.js
+++ javascript.js
@@ -1,65 +1,33 @@
-// Defining Multiple Properties
-// It’s also possible to define multiple properties on an object simultaneously
-// Object.defineProperties()
+///// now we will create an object /// with data and accessor properties using defineProperties method 
 
-
-
-
-var person = {
-
-};
-
-/// 2 args remember --- for defineProperty 3 args - the third arg being the property descriptor 
-
-// This
-// method accepts two arguments: the object to work on and an object containing
-// all of the property information
-
-
-// The keys of that second argument
-// are property names, and the values are descriptor objects defining the
-// attributes for those properties
-
-
-//Object.defineProperties(person, "name") /// wrong // 2nd arg itself an object with keys and values 
-
-Object.defineProperties(person, {
-	// adding data property name and age
-	name : {
-		value :"srini", 
-		enumerable : true, 
-		//configurable : true
-		writable : true
-	}, 
-	age :{
-		value :25, 
-		enumerable : true, 
-		configurable : true,
-		writable : true
-	}
+var person1 = {};
+Object.defineProperties(person1, {
+ // data property to store data
+_name: {
+value: "Nicholas",
+enumerable: true,
+configurable: true,
+writable: true
+},
+// accessor property
+name: {
+get: function() {
+console.log("Reading name");
+return this._name;
+},
+set: function(value) {
+console.log("Setting name to %s", value);
+this._name = value;
+},
+enumerable: true,
+configurable: true
+}
 });
 
-console.log(person);  //Object {name: "srini", age: 25}
-console.log(Object.getOwnPropertyDescriptor(person, "name"));
-
-// now writable is true, but configurable is false 
-
-person.name = "boo";    
-console.log(person.name);  // prints boo since writable is true 
-
-/// we will see configurable now 
-
-
-console.log(delete person.name);  // returns false since configurable is false
-
-console.log("name" in person);  //// true , name still exists in person
-
-console.log(person.name);  //// boo
-
-
-
-
-//Object {value: "srini", writable: true, enumerable: true, configurable: true}
-console.log(Object.getOwnPropertyDescriptor(person, "age"));
-
-//Object {value: 25, writable: true, enumerable: true, configurable: true}
+// This example defines _name as a data property to contain information
+// and name as an accessor property. You can define any number
+// of properties using Object.defineProperties();
+// you can even change existing
+// properties and create new ones at the same time. The effect is the same
+// as calling Object.defineProperty()
+// multiple times.
