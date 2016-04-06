b23a8dfd6fa34e08e40da19a7fd274b69e5db2b3
srini88 <pvsrinivas.ece@gmail.com>
2016-04-06 14:29:11 -0500

    enumerable more

--- javascript.js
+++ javascript.js
@@ -1,8 +1,6 @@
 ///moving on
 
 
-
-
 var person = {
 	name : "Srini", 
 	age: 23
@@ -14,6 +12,17 @@ console.log(Object.getOwnPropertyDescriptor(person, "name"));
 //Object {value: "Srini", writable: true, enumerable: true, configurable: true}
 
 // enumerable and cofigurale are set to true 
+
+
+// If you want to change property attributes, you can use the Object
+// .defineProperty() method. This method accepts three arguments: the
+// object that owns the property, the property name, and a property descriptor
+// object containing the attributes to set. The descriptor has properties
+// with the same name as the internal attributes but without the square
+// brackets. So you use enumerable
+// to set [[Enumerable]], and configurable
+// to set [[Configurable]].
+
 
 // now lets change them to false- you could change writable too
 Object.defineProperty(person, "name", {
@@ -28,10 +37,16 @@ console.log(Object.getOwnPropertyDescriptor(person, "name"));
 // you could see the changes now 
 //Object {value: "Srini", writable: true, enumerable: false, configurable: false} 
 
-///// SINCE configurable is set to false 
+/// lets deal with enumerable now 
 
-console.log(delete person.name);  //false
-console.log(person); // Object {age: 23, name: "Srini"} // still prints name 
+var arr = Object.keys(person);
+console.log(arr);  //["age"] /// because name is not enumerable
+// checking it second way 
+
+console.log(person.propertyIsEnumerable("name"));  /// false - not enumerable
+
+console.log("name" in person);  //   true - still there but not enumerable
+
 
 
 
