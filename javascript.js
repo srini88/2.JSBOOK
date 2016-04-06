cf966e18ca12cac515458c2f4f4901bac11e9745
srini88 <pvsrinivas.ece@gmail.com>
2016-04-06 14:36:35 -0500

    non confg to configurable

--- javascript.js
+++ javascript.js
@@ -5,56 +5,41 @@ var person = {
 	name : "Srini", 
 	age: 23
 };
-
-//console.log(person);
-
 console.log(Object.getOwnPropertyDescriptor(person, "name"));
-//Object {value: "Srini", writable: true, enumerable: true, configurable: true}
-
-// enumerable and cofigurale are set to true 
 
 
-// If you want to change property attributes, you can use the Object
-// .defineProperty() method. This method accepts three arguments: the
-// object that owns the property, the property name, and a property descriptor
-// object containing the attributes to set. The descriptor has properties
-// with the same name as the internal attributes but without the square
-// brackets. So you use enumerable
-// to set [[Enumerable]], and configurable
-// to set [[Configurable]].
+// Object {value: "Srini", writable: true, enumerable: true, configurable: true}
 
 
-// now lets change them to false- you could change writable too
+// changin  confgiurable to false 
 Object.defineProperty(person, "name", {
 
 
-	enumerable: false, 
-	configurable:false
+	configurable : false
+
+});
+console.log(Object.getOwnPropertyDescriptor(person, "name"));
+
+//Object {value: "Srini", writable: true, enumerable: true, configurable: false}
+
+//// now lets change it back to true
+
+
+Object.defineProperty(person, "name", {
+
+	configurable : true
 
 });
 
-console.log(Object.getOwnPropertyDescriptor(person, "name"));
-// you could see the changes now 
-//Object {value: "Srini", writable: true, enumerable: false, configurable: false} 
+console.log(Object.getOwnPropertyDescriptor(person, "name")); ////Uncaught TypeError: Cannot redefine property: name
 
-/// lets deal with enumerable now 
+/// error can't change non configurable to configurable again 
 
-var arr = Object.keys(person);
-console.log(arr);  //["age"] /// because name is not enumerable
-// checking it second way 
+// After that, name is changed to be nonconfigurable x. From now on,
+// attempts to delete name fail because the property can’t be changed, so
+// name is still present on person1
 
-console.log(person.propertyIsEnumerable("name"));  /// false - not enumerable
-
-console.log("name" in person);  //   true - still there but not enumerable
-
-
-
-
-
-
-
-
-
-
+/// Effectively,
+// name is locked down as a property on person1.
 
 
