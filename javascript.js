edb86f221c6e6c6b0c8eab5928041b086bafbab2
srini88 <pvsrinivas.ece@gmail.com>
2016-04-06 14:57:48 -0500

    writable is a subset of configurabel

--- javascript.js
+++ javascript.js
@@ -1,37 +1,32 @@
-//////Data propert attribuutes
+//// difference between configurable and writable 
+/// these are called property description
 
-//// data porperties possess two additional attributes that accesors do not
-/// value
-////The first is [[Value]], which holds the property value. This attribute is
-// filled in automatically when you create a property on an object. All property
-// values are stored in [[Value]], even if the value is a function.
-// The second attribute is [[Writable]], which is a Boolean value indicating
-// writable 
+// Writable: If false, the value of the property can not be changed.
 
+// Configurable: If false, any attempts to delete the property or change its attributes (Writable, Configurable, or Enumerable) will fail.
 
-/// bby default all properties are writable 
+// Enumerable: If true, the property will be iterated over when a user does for (var prop in obj){} (or similar).
+
 
 var person = {
 	name :"srini"
 };
 
 console.log(Object.getOwnPropertyDescriptor(person, "name"));
-//// Object {value: "srini", writable: true, enumerable: true, configurable: true}
-
-// you see both value and writable 
-///// 2nd way of definignn the same object 
-
-var person1 = {};
-
-Object.defineProperty(person1, "name", {
-
-	value : "srini"
+Object.defineProperty(person, "name", {
+	writable :false
 });
 
-console.log(Object.getOwnPropertyDescriptor(person1, "name"));
-//// Object {value: "srini", writable: false, enumerable: false, configurable: false}, the others defaulted to false 
+// changing to false 
+console.log(Object.getOwnPropertyDescriptor(person, "name"));
 
-// When Object.defineProperty() is called, it first checks to see if the
-// property exists. If the property doesn’t exist, a new one is added with
-// the attributes specified in the descriptor. In this case, name isn’t already
-// a property of person1, so it is created.
+console.log(Object.keys(person));  //["name"]
+
+
+console.log(delete person.name);  // true 
+//// this is deleted even though writable is set to false, 
+/// in case of configurable you cannot delete it to o
+
+/// writable is a subset of configurable 
+
+console.log(Object.keys(person));  //[]
