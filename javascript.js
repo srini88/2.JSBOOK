e84964c8e943ed2b1739290502448b0c6a952864
srini88 <pvsrinivas.ece@gmail.com>
2016-04-06 13:53:46 -0500

    deleting prop that is not configurable

--- javascript.js
+++ javascript.js
@@ -1,47 +1,45 @@
-////moving on
-//////PROPERTY attributes 
-
-//// before ECMA5 , no way to specify wheterh a proerty is enumberable 
-/// no way to access internal attributes of a property at al l 
+///moving on
 
 
-/// learning about attributes of both data and accessor properties 
 
-//// the common ones 
-
-/// enumerable, configurable 
-
-// There are two property attributes shared between data and accessor
-// properties. One is [[Enumerable]], which determines whether you can
-// iterate
-// over the property. The other is [[Configurable]], which determines
-// whether the property can be changed.
-
-
-//configurable means you can delete that godammn property, add shit and all,  if it is not configurable you cannot do anything 
-
-
-// If you want to change property attributes, you can use the Object
-// .defineProperty() method.
 
 var person = {
 	name : "Srini", 
 	age: 23
 };
 
-console.log(person);
+//console.log(person);
 
-// The Object.getOwnPropertyDescriptor() method returns a property descriptor for an own property (that is, one directly present on an object, not present by dint of being along an object's prototype chain) of a given object.
+console.log(Object.getOwnPropertyDescriptor(person, "name"));
+//Object {value: "Srini", writable: true, enumerable: true, configurable: true}
 
-var obj1 = Object.getOwnPropertyDescriptor(person, "name");  /// returns an object 
-console.log(obj1);  //Object {value: "Srini", writable: true, enumerable: true, configurable: true}
+// enumerable and cofigurale are set to true 
 
-// now we will look at the age property
-var obj2 = Object.getOwnPropertyDescriptor(person, "age");
-console.log(obj2);  //Object {value: 23, writable: true, enumerable: true, configurable: true}
+// now lets change them to false- you could change writable too
+Object.defineProperty(person, "name", {
 
-/// as you can see it has the value of 23, remember the key "Value"
 
-var obj = Object.getOwnPropertyNames(person);  //// easier to use than using for in or the Object.keys 
+	enumerable: false, 
+	configurable:false
 
-console.log(obj);  //["name", "age"]
+});
+
+console.log(Object.getOwnPropertyDescriptor(person, "name"));
+// you could see the changes now 
+//Object {value: "Srini", writable: true, enumerable: false, configurable: false} 
+
+///// SINCE configurable is set to false 
+
+console.log(delete person.name);  //false
+console.log(person); // Object {age: 23, name: "Srini"} // still prints name 
+
+
+
+
+
+
+
+
+
+
+
