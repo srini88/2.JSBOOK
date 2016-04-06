468e3034ba266b537b42271ddd89ebf2c837ca36
srini88 <pvsrinivas.ece@gmail.com>
2016-04-06 10:27:44 -0500

    deleting property

--- javascript.js
+++ javascript.js
@@ -1,157 +1,81 @@
-///what is “strict mode” and how is it used?
+//"use strict";   // use strict should be at the top 
+//console.log(delete Object.prototype); // uncaught exception
 
+//"use strict";  without strict
+console.log(delete Object.prototype); //false , without strict 
 
-http://stackoverflow.com/questions/8651415/what-is-strict-mode-and-how-is-it-used
 
-ECMAScript5 introduced strict mode 
 
 
-Its main purpose is to do more checking.
+// Say I create an object as follows:
 
-Just add "use strict"; at the top of your code, before anything else.
+var myObject = {
+	"ircEvent": "PRIVMSG",
+	 "method": "newURI", 
+	 "regex": "^http://.*"
+	};
+// What is the best way to remove the property regex to end up with new myObject as follows?
 
-For example, blah = 33; is valid JavaScript. It means you create a completely global variable blah.
+// var myObject = {"ircEvent": "PRIVMSG", "method": "newURI"};
 
-But in strict mode its an error because you did not use the keyword "var" to declare the variable.
 
-Most of the time you don't mean to create global variables in the middle of some arbitrary scope, so most of the time that blah = 33 is written it is an error and the programmer didn't actually want it to be a global variable, they meant to write var blah = 33.
+console.log(myObject);
+delete myObject.regex;   // deletes it 
 
-It similarly disallows a lot of things that are technically valid to do. NaN = "lol" does not produce an error. It also doesn't change the value of NaN. using strict this (and similar weird statements) produce errors. Most people appreciate this because there is no reason to ever write NaN = "lol", so there was most likely a typo.
 
+console.log(myObject);
 
-Strict mode was also designed in the hope that developers who limit themselves to strict mode would make fewer mistakes and that the bugs they do make would manifest in more obvious ways.
+/////Other ways to delete 
 
+delete myObject.regex;
+// or,
+delete myObject['regex'];
+// or,
+var prop = "regex";
+delete myObject[prop];
 
-strict mode changes silent errors to throw errors 
 
-Strict mode makes several changes to normal JavaScript semantics.
+http://stackoverflow.com/questions/208105/how-do-i-remove-a-property-from-a-javascript-object
 
-strict mode eliminates some JavaScript silent errors by changing them to throw errors.
+http://perfectionkills.com/understanding-delete/
 
-strict mode fixes mistakes that make it difficult for JavaScript engines to perform optimizations.
 
-strict mode prohibits some syntax likely to be defined in future versions of ECMAScript.
+a property is assigned to undefined still is a property of an object, so it will not be removed by GC The GC doesn't manage anything about properties
 
-ECMAScript5 introduces some new objects and properties and also the so-called "strict mode"
 
 
-Invoking Strict Mode in Code
 
-Strict mode applies to entire scripts or to individual function. It doesn’t apply to block statement enclosed in {} braces, attempting to apply it to such contexts does nothing.
 
-Entire Script:
+Operator delete is unexpectedly slow!
+Look at the benchmark.
 
-Let say we are creating app.js so adding first statement use script will enforce strict mode for entire code.
+Delete is the only true way to remove object's properties without any leftovers, but it works ~ 100 times slower, compared to it's "alternative", setting object[key] = undefined.
 
-// app.js whole script in strict mode syntax
-“use strict”;
-// Now you can start writing your code 
-Strict mode for function:
+This alternative is not the correct answer to this question! But, if you use it with care, you can dramatically speed up some algorithms. If you are using delete in loops and you have problems with performance, read the verbose explanation.
 
-To Invoke strict mode for a function, put the exact statement “use strict”; in the start of function body before any other statement.
+When should one use delete and when set value to undefined ?
+An object may be seen as a set of key-value pairs. What I call a 'value' is a primitive or a reference to other object, connected to that 'key'.
 
-function yourFunc(){
- "use strict";
+Use delete, when you are passing the result object to the code on which you don't have control (or when you are not sure about your team or yourself).
 
- // Your function code logic
-}
-Strict mode incorporate several changes to normal Javascript semantics. First strict mode eliminate some JavaScript silent error by changing them to throw errors.
+It deletes the key from the hashmap.
 
-For Instance: Code using Strict Mode
+ var obj = {
+     field: 1     
+ };
+ delete obj.field;
+Use setting to undefined, when you care about performance. It can give a serious boost to your code.
 
-enter image description here
+The key remains on its place in the hashmap, only the value is replaced with undefined. Understand, that for..in loop will still iterate over that key.
 
-In above code example without using strict mode in code It won't throw an error. As we are accessing variable x without declaring it. So in strict mode accessing undeclared variable throw an error.
+ var obj = {
+     field: 1     
+ };
+ obj.field = undefined;
+Using this method, not all ways of determining property existence will work as expected.
 
-Now let's try to access variable x without declaring it without strict mode.
+However, this code:
 
-(function(){
-    x = 3;
-})();
+object.field === undefined
 
-// Will not throw an error
-
-
-
-
-
-
-
-
-With strict mode, you can not, for example, use undeclared variables.
-
-Declared at the beginning of a JavaScript file, it has global scope (all code will execute in strict mode):
-
-Example
-"use strict";
-x = 3.14;       // This will cause an error (x is not defined)
-
-
-
-"use strict";
-myFunction();
-
-function myFunction() {
-    y = 3.14;   // This will also cause an error (y is not defined)
-}
-
-
-Declared inside a function, it has local scope (only the code inside the function is in strict mode):
-
-x = 3.14;       // This will not cause an error. 
-myFunction();
-
-function myFunction() {
-   "use strict";
-    y = 3.14;   // This will cause an error (y is not defined)
-}
-
-
-Strict mode changes previously accepted "bad syntax" into real errors.
-
-
-Using an object, without declaring it, is not allowed:
-
-"use strict";
-x = {p1:10, p2:20};      // This will cause an error (x is not defined)
-
-
-Deleting a variable (or object) is not allowed.
-
-"use strict";
-var x = 3.14;
-delete x;                // This will cause an error
-
-
-Deleting a function is not allowed.
-
-"use strict";
-function x(p1, p2) {}; 
-delete x;                // This will cause an error 
-
-
-"use strict";
-var obj = {};
-Object.defineProperty(obj, "x", {value:0, writable:false});
-
-obj.x = 3.14;            // This will cause an error
-
-"use strict";
-delete Object.prototype; // This will cause an error
-
-
-Future reserved keywords are not allowed in strict mode. These are:
-
-implements
-interface
-let
-package
-private
-protected
-public
-static
-yield
-
-
-"use strict";
-var public = 1500;      // This will cause an error
+will behave equivalently for both methods.
