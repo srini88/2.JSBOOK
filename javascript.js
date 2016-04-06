a4811d9262630e790960f3a021c545fec2955be5
srini88 <pvsrinivas.ece@gmail.com>
2016-04-06 16:34:04 -0500

    getOwnPropertyDescriptor

--- javascript.js
+++ javascript.js
@@ -1,33 +1,12 @@
-///// now we will create an object /// with data and accessor properties using defineProperties method 
+//////retriving property attributes 
 
-var person1 = {};
-Object.defineProperties(person1, {
- // data property to store data
-_name: {
-value: "Nicholas",
-enumerable: true,
-configurable: true,
-writable: true
-},
-// accessor property
-name: {
-get: function() {
-console.log("Reading name");
-return this._name;
-},
-set: function(value) {
-console.log("Setting name to %s", value);
-this._name = value;
-},
-enumerable: true,
-configurable: true
-}
-});
+var person1 = {
+name: "Nicholas"
+};
+var descriptor = Object.getOwnPropertyDescriptor(person1, "name");
+console.log(descriptor.enumerable); // true
+console.log(descriptor.configurable); // true
+console.log(descriptor.writable); // true
+console.log(descriptor.value); // "Nicholas"
 
-// This example defines _name as a data property to contain information
-// and name as an accessor property. You can define any number
-// of properties using Object.defineProperties();
-// you can even change existing
-// properties and create new ones at the same time. The effect is the same
-// as calling Object.defineProperty()
-// multiple times.
+
