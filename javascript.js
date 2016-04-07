fa1a134df972f29e22cf7952f468598b60e52651
srini88 <pvsrinivas.ece@gmail.com>
2016-04-06 22:50:27 -0500

    constructor accessor

--- javascript.js
+++ javascript.js
@@ -1,13 +1,24 @@
-var arr = [];
+////constructors allow you to initialize an instance of a tuype in a consistent way.... performing all the property setup that is necessary before the object can be used...
 
-console.log(arr.constructor);  //function Array() { [native code] }
+//// you can also use Object.defineProperty() inside a constructor to helop initialize the instance 
 
-console.log(delete arr.constructor);   //true
+function Person(name) {
+Object.defineProperty(this, "name", {
+get: function() {
+return name;
+},
+set: function(newName) {
+name = newName;
+},
+enumerable: true,
+configurable: true
+});
+this.sayName = function() {
+console.log(this.name);
+};
+}
 
-/// deletig, but still the below statemtn give s
-console.log(arr.constructor);  //function Array() { [native code] }
-
-console.log(Array.constructor);   /// function Function() { [native code] }
-
-console.log(Function.constructor);/// function Function() { [native code] }
-
+// In this version of the Person constructor, the name property is an
+// accessor
+// property that uses the name parameter for storing the actual
+// name. This is possible because named parameters act like local variables.
