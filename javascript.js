af5a0ba71379a86d254cdcaff578d5f2c54f9ee9
srini88 <pvsrinivas.ece@gmail.com>
2016-04-07 10:35:43 -0500

    without new on a constructor

--- javascript.js
+++ javascript.js
@@ -1,24 +1,37 @@
-////constructors allow you to initialize an instance of a tuype in a consistent way.... performing all the property setup that is necessary before the object can be used...
+//"use strict";  with use strict  the program wont execute
+//// Cannot set property 'name' of undefined
+// An error occurs if you call the Person constructor in strict mode without using new.
+// This is because strict mode doesn’t assign this to the global object. Instead, this
+// remains undefined, and an error occurs whenever you attempt to create a property
+// on undefined.
 
-//// you can also use Object.defineProperty() inside a constructor to helop initialize the instance 
 
-function Person(name) {
-Object.defineProperty(this, "name", {
-get: function() {
-return name;
-},
-set: function(newName) {
-name = newName;
-},
-enumerable: true,
-configurable: true
-});
-this.sayName = function() {
-console.log(this.name);
-};
+function Person(name){
+	this.name = name ; 
 }
 
-// In this version of the Person constructor, the name property is an
-// accessor
-// property that uses the name parameter for storing the actual
-// name. This is possible because named parameters act like local variables.
+var p1 = new Person("srini");
+var p2 = new Person("vas");
+
+console.log(p1.name);  //srini
+console.log(p2.name);  //vas
+
+
+
+//// always call construcors with new 
+
+var p3 = Person("pvs");   // new is not passed , the value of "this" inside the construcot is equal to the globla "this"
+/// Also person does not return anything.... so p3 is undefined
+
+
+
+///// this p3 does not contain a value bcoz the Person constructor relies on new to supply a return value
+
+console.log(p3);  ///undefined
+
+// Without new, Person is just a function without
+// a return statement
+
+console.log(name);  /// pvs - calling without object - works because name is global --- means we set window.name 
+
+console.log(window.name);   ///pvs 
