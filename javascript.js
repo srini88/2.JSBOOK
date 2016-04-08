4b74027c8f4a3b7265f54f80e04a02853b41357d
srini88 <pvsrinivas.ece@gmail.com>
2016-04-07 22:15:22 -0500

    prototype  and proto

--- javascript.js
+++ javascript.js
@@ -1,45 +1,44 @@
+////The [[prototype ]] property 
+
+
+//// an instance keeps track of its prototype through an internal propterty called [[prototype]]
+
+////this property is a pointer back to the prototype object that the instance is using
+
+//// when you create a new object using new, the constroctors prototype property is assigned to the [[prototype]] property of the new object 
+
+/// the [[prototype]] property lets multiple instances of an object type refert to the same prototype
+
+
+// var p1 = new person;
+// var p2 = new person;
+
+
+//both p1 and p2's [[prototype]] property point to the person.prototype
+
+
+/////how will you read the [[prototype ]] property
+////use Object.getPrototypeOf(obj) method 
 
 
 
-/////how to determine if a godammn function is on the prototpye and not in the object instance
+var obj = {};
 
-var person = {
-	name :"srinivas", 
-	age : 21
-};
+//this obj's [[prototype]] property shoudl point to Object.prototype
 
 
-console.log("name" in person);  //true   //own property
-console.log("toString" in person);  ///true //prototype property
+proto = Object.getPrototypeOf(obj);
 
-console.log(person.hasOwnProperty("name"));   ///true
-console.log(person.hasOwnProperty("toString"));  //false
+console.log(proto);   //Object{}
 
+console.log(proto === Object.prototype);       //true there you go 
+////for any generic object liek this one [[prototype]] always refer to Object.prototype
 
-// it is in prototype
-
-// function to determine if a property is on the prototype
-/// if it is in the prototype - returns true, else false
-function determine(obj, prop){
-
-	/// in lo undi  and  hasOwnProperty lo lekapothe - it is in prototype
-	if ((prop in obj) && !(obj.hasOwnProperty(prop)))
-		return true;
-
-	return false;
-}
-
-// check if toString is in prototype , should return true 
+console.log(proto ===Object);/////false   ///proto points to Object.prototype not Object itself
 
 
-console.log(determine(person, "toString"));  //true
-
-// name is own, should return false 
-
-console.log(determine(person, "name"));  //false
-
-
-
+/// some JS engines support a property called __proto__ on all objects..
+///// this property allows you to both read from and write to the [[prototype]] property....
 
 
 
