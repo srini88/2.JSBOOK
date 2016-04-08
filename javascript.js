c4e221edcb8b11a0afa7b91a66b617e614b8f346
srini88 <pvsrinivas.ece@gmail.com>
2016-04-07 23:41:51 -0500

    protosdf

--- javascript.js
+++ javascript.js
@@ -1,42 +1,46 @@
-///////Using prototypes with constructors 
-
-//////the shared nature of protopes makes them ideal for defining methods once for all objects of a given type
-
-/////// methods tend to do the sma ething for all instances (instance properties may be different, something like sayName is same for all instances ), theere is no reason each instance needs its own set of methods 
-
-///// much more efficient to put the methods on the prototype and then use "this" to access the current instance....
-
-
 function Person(name){
 	this.name = name;
 }
 
-//// defining sayName on the prototype instead of in the constructor...
-/////because person1 and person2 are  each base references for their calls to sayName(), the "this" value is assigned to person1 and person2 respectively
-Person.prototype.sayName = function(){
-	console.log(this.name);
-};
+
+// Person.prototype.sayName = function(){
+// 	console.log(this.name);
+// };
+
+//// you can store other types of data on the prototype, but be careful when using reference values.... 
+//// these godammn values are shared across instances, 
+///// one instance change value on the prototype method or something, 
+//// the other instance can see too
+
+//// now instand of sayName we will add a godammn array 
+
+Person.prototype.favorites = [];
 
 var person1 = new Person("srini");
 var person2 = new Person("vas");
 
-console.log(person1.name);  //srini
-console.log(person2.name); //vas
 
-person1.sayName(); //srini
-person2.sayName(); ///vas
+person1.favorites = ["coke"];
+person2.favorites = ["pepsi"];
 
-
-console.log(person1 instanceof Person);  //true
-console.log(Person instanceof Function);  //true
-
-console.log(person1.constructor);   //function Person(mame {})
-
-console.log(person1.constructor === Person);  ///points to the constructor Person
-
-var person1proto = Object.getPrototypeOf(person1);
-
-console.log(person1proto ===Person.prototype);   ///true
+console.log(person1.favorites);  //["coke"]
+console.log(person2.favorites);  //["pepsi"]
 
 
-//////// above on the godammn prototype we added a method ,  which is the same godammn method for both the instances ,,, no two copies ....
+// I expected both the log statements to print ["pepsi"] since both person1 and person2 point to the same base array favorites on the prototype, and since person2 modified it at last to ["pepsi"], I expected the both the outputs to be ["pepsi"]. 
+
+
+// You can append to an array using push and you can empty an array by setting its length to 0. As an example:
+
+person1.favorites.push( "boo" );   /// we are pushing to person1 instance// not prototype ones
+//person2.favorites.length = 0;        
+person2.favorites.push( "foo" );/// we are pushing to person1 instance// not prototype ones
+
+
+console.log(person1.favorites);  //["coke"]
+console.log(person2.favorites);  //["pepsi"]
+
+///answer
+
+////check my question
+//http://stackoverflow.com/questions/36491463/updating-a-prototype-using-an-instance-in-javascript/36491505?noredirect=1#comment60591402_36491505
