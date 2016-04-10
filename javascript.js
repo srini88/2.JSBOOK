096cbdcb09068c48b269f34c0625c9d043f8c56d
srini88 <pvsrinivas.ece@gmail.com>
2016-04-10 16:41:42 -0500

    prototype frozen

--- javascript.js
+++ javascript.js
@@ -1,41 +1,35 @@
+///// what happens if you add method to prototype and the instance is sealed
 
-//// changing prototypes 
+///sealed and frozen objects 
+/// when using Object.seal or Object.freeze on an object .. you are acting soleely on the object instances and the own propertyies
+//// you cant add new own properties or change existing own propertioes on frozen objects
+/// but you can certainly still add properties on the prototupe and continue extending those  objects 
 
-//// all instances of a particular type reference a shared prototype.... 
-///the [[prototype]] property just contains a pointer to the prototype and any changes to the prototype are immediately available on any instance referencing it
-
-//// literally add new mmeberts to a prototype at any point and ahve those changes reflected on existing instances 
-/// same as the previous one
 
 function Person(name) {
 	this.name = name;
 }
-Person.prototype = {
-	constructor: Person,
- 	sayName: function() {
-		console.log(this.name);
-	},
- 	toString: function() {
-	return "[Person " + this.name + "]";
-	}
-};
 
-var person1 = new Person("Nicholas");
+var person1 = new Person("Nicho");
 var person2 = new Person("Greg");
 
-console.log("sayHi" in person1);  ///not there in both person1 and 2 //false
-console.log('sayHi' in person2);
 
-//// lets add that sayHi to the prototype itself
+Object.freeze(person1);
 
-Person.prototype.sayHi = function (){
+Person.prototype.sayHi = function(){
 	console.log("Hi");
 };
 
-person1.sayHi();
-person2.sayHi();
+person1.sayBoo = function(){
+	console.log("sayBoo");
+}
+person1.sayHi();  //hi  - still outputs Hi eventhoug frozen
+person1.sayBoo();  ///Uncaught TypeError: person1.sayBoo is not a function //// cannot add on teh instance itself if frozen, but can add on the prototype
+person2.sayHi(); //hi 
 
-console.log(person1.hasOwnProperty("sayHi"));   //false /// not own property/// as you can see it is on the Prototype property
 
 
+/// when adding sayHi to the prototype, both person1 and person2 attain a new method, --- seemingly contradicting person1's frozen status 
+
+/// the [[prototype]]property is considered an own property of an instance, and whiel the property itself is frozen, the value (an object) is not
 
