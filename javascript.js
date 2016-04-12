20dad20f621c890014f6be1bd7854b384e386e04
srini88 <pvsrinivas.ece@gmail.com>
2016-04-12 16:55:11 -0500

    chumma example

--- javascript.js
+++ javascript.js
@@ -1,51 +1,18 @@
-
-var person = {
-
+function D(drink) {
+	this.drink = drink;
 };
 
-//// ddefineProperties only 2 arguments - the second one being an object
 
-Object.defineProperties(person, {
-
-	name : {
-		value:"Srini", 
-		
-		configurable :true, 
-		writable :true
-
-	}, 
-	job: {
-		value : "JS developer",
-		enumerable :true, 
-		configurable :true, 
-		writable :true
+D.prototype = {
+	getDrink: function(){
+		return this.drink;
 	}
-});
+};
+
+var d = new D("coke");
+
+console.log(d.getDrink());  //coke
 
 
-console.log(person);   //Object {name: "Srini", job: "JS developer"}
-
-
-
-var keys = Object.keys(person);
-console.log(keys);  //["name", "job"]
-
-// now lets change enumerable to false on name, 
-//// we will see with getOwnpropertyNames
-var prop = Object.getOwnPropertyNames(person);
-console.log(prop);  //["name", "job"]  - brints both even though enumerable is false on Name ---- know the difference now 
-
-
-var desc = Object.getOwnPropertyDescriptor(person, "name"); ///this returns an object
-console.log(desc);   //Object {value: "Srini", writable: true, enumerable: false, configurable: true}
-
-
-var keys1 = Object.keys(person);
-
-console.log(keys1); //["job"]  ///not printing name , because not enumerable
-
-
-////////lets see prototpye and constructor
-
-console.log(person.constructor);  ///function Object(){native shit}
-console.log(person.prototype);  ////undefined now 
+console.log(d.constructor === D);  //false
+console.log(d.constructor === Object);  //true === pointing to Object insted -- constructor property is overridded
