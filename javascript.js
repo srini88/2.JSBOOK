617226e133ff9ce89a5d03a0f589fecd3f9001a6
srini88 <pvsrinivas.ece@gmail.com>
2016-04-12 17:12:51 -0500

    other:javascript delete

--- javascript.js
+++ javascript.js
@@ -1,18 +1,33 @@
-function D(drink) {
-	this.drink = drink;
-};
+
+///http://perfectionkills.com/understanding-delete/
+
+var b = 5;
+
+delete b;   ///does not work   ///returns false
+
+console.log(b);   ////// still prints 5
 
 
-D.prototype = {
-	getDrink: function(){
-		return this.drink;
-	}
-};
+a = 2;
 
-var d = new D("coke");
-
-console.log(d.getDrink());  //coke
+delete a;   //works
+console.log(a);  ///a is not defined
 
 
-console.log(d.constructor === D);  //false
-console.log(d.constructor === Object);  //true === pointing to Object insted -- constructor property is overridded
+The point is the delete operator removes a property from an object. It cannot remove a variable. So the answer to the question depends on how the global variable or property is defined.
+
+(1) If it is created with var, it cannot be deleted.
+
+For example:
+
+var g_a = 1; //create with var, g_a is a variable 
+delete g_a; //return false
+console.log(g_a); //g_a is still 1
+(2) If it is created without var, it can be deleted.
+
+g_b = 1; //create without var, g_b is a property 
+delete g_b; //return true
+console.log(g_b); //error, g_b is not defined
+
+
+http://stackoverflow.com/questions/1596782/how-to-unset-a-javascript-variable
