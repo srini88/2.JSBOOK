311d16c3cb40dd723a5e340d94ffcbf3e10d19af
srini88 <pvsrinivas.ece@gmail.com>
2016-04-12 17:31:19 -0500

    other iffe basics

--- javascript.js
+++ javascript.js
@@ -1,33 +1,63 @@
+var a = (function(x){
+    delete x;
+    return x;
+})(1);
 
-///http://perfectionkills.com/understanding-delete/
-
-var b = 5;
-
-delete b;   ///does not work   ///returns false
-
-console.log(b);   ////// still prints 5
+console.log(a);  //prints 1
 
 
-a = 2;
+//http://stackoverflow.com/questions/9053842/advanced-javascript-why-is-this-function-wrapped-in-parentheses?lq=1
 
-delete a;   //works
-console.log(a);  ///a is not defined
+///mmediately-invoked function expressions can be used to avoid variable hoisting from within blocks, protect against polluting the global environment and simultaneously allow public access to methods while retaining privacy for variables defined within the function. This pattern has been referred to as a self-executing anonymous function,[2] but Ben Alman introduced the term IIFE as a more semantically accurate term for the pattern, shortly after its discussion arose on comp.lang.javascript
 
+(function() {
+  // some code
+})();
 
-The point is the delete operator removes a property from an object. It cannot remove a variable. So the answer to the question depends on how the global variable or property is defined.
+// This provides a way to execute some JS code in it's own scope. It's usually used so that any variables created within the function won't affect the global scope
 
-(1) If it is created with var, it cannot be deleted.
-
-For example:
-
-var g_a = 1; //create with var, g_a is a variable 
-delete g_a; //return false
-console.log(g_a); //g_a is still 1
-(2) If it is created without var, it can be deleted.
-
-g_b = 1; //create without var, g_b is a property 
-delete g_b; //return true
-console.log(g_b); //error, g_b is not defined
+function foo() {
+  // some code
+}
+foo();
 
 
-http://stackoverflow.com/questions/1596782/how-to-unset-a-javascript-variable
+// But this requires giving a name to the function, which is not always necessary. Using a named function also means at some future point the function could be called again which might not be desirable. By using an anonymous function in this manner you ensure it's only executed once.
+
+
+// by using an anonymous function in this manner you are ensuring it is only executed once....  executed only once
+
+
+/// the below syntax is invalid   ---- Because you have to wrap the function in parens in order to make it parse as an expression. More info here
+function() {
+  // some code
+}();
+
+///The below is IIFE
+(function(){
+
+})();
+
+// Allows 'some code' to be executed immediately, as if it was just written inline, but also within its own scope so as not to affect the global namespace (and thus potentially interfere with or be interfered with by, other scripts).
+
+// You can pass arguments to your function just as you would a normal function, eg
+
+(function(x) {
+  // some code
+})(1);
+
+
+// Three more common wordings:
+
+// Crockford's preference - parens on the inside
+(function() {
+  console.log('Welcome to the Internet. Please follow me.');
+}());
+
+(function() {
+  console.log('Welcome to the Internet. Please follow me.');
+})();
+
+!function() {
+  console.log('Welcome to the Internet. Please follow me.');
+}();
