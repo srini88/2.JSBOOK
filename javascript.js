9bf9a599536840e726761502e1169e19875ba6f1
srini88 <pvsrinivas.ece@gmail.com>
2016-04-14 22:38:41 -0500

    valueof and toString

--- javascript.js
+++ javascript.js
@@ -1,45 +1,70 @@
-///in traditional oo languages, classes inherit properties from other classess....
-
-// In JS, inheritance,  occurs betweenobjects with no class like structure defining the relationship
-// through rptotypes
-
-// prototype chaining and Object.prototype
-
-
-// Javascriopt built in approach for inheritance is called prototype chaining or prototypal inheritance 
-
-// prototype properties are automatically available on object instances...
-// which is form of inheritance...
-
-
-// because the prototype is also an object, it has its own prototype and inherits from that... this is the prototype chain ....
-
-
-
-// all generic shit inherit from Object.prototype
-
-// any object defined via an object literal has its [[prototype ]] set to object.prototype 
-
-var book  = {
-	title: "HEE"    ///no semicolon here..just one propertty
-};
-
-
-var bookproto = Object.getPrototypeOf(book);
-
-console.log(bookproto === Object.prototype);  ///true 
-
-// means book automatically receives methods from Object.prototype 
-
-// Shit that is on Object.prototype 
-
-// hasOwnProperty(property)
-
-// propertyIsEnumerable(prop
-
-// 	getPrototypeOf(Object))
 // valueOf()
 
-// toString()
+// this gets alled wheneveer an operator is used on an object...
+// by default it simply returns an object instance....
 
-These methods appoear on all the objects through inheritance....
+// the primitive wrapper types override valueOf so that it returns a string for String, boolean for a boolean and a number for Number.... 
+
+// Date objects valueof method returns the epoch time in milliseconds...just as Date.prototype.getTime() does.... 
+
+
+
+var now = new Date;
+
+var earlier = new Date(2010, 1, 1);
+
+console.log(now);  //Thu Apr 14 2016 22:29:50 GMT-0500 (Coordinated Universal Time)
+console.log(earlier);  ///Mon Feb 01 2010 00:00:00 GMT-0600 (Coordinated Universal Time)
+
+
+console.log(now - earlier);   //195687009977
+
+console.log(now > earlier);  //// true 
+
+// above, the valueOf is called on both objects before the - or comparison is performed... 
+// in the subtraction you got the difference in epoch time because of Valueof 
+
+
+/////you can always define your own valueOf method if your objects are intended to be used with operators .....
+///toString() 
+
+/// toString() method is called as a fallback whenever valueOf returns a reference value instead of a primitive value 
+///it is also implicity called on primitive values when JS is expecting a string...
+
+///ex when string is used as one operand for the plus operator, the other oeprand is automatically converted to a string... 
+
+///if the other operand is a primitive value, it is converted into a string representation -- (true becomes "true")
+
+
+////if it is a reference value , then valueOf is called...
+
+// if valueOf returns a reference value, toString is called...
+
+var book = {
+
+	title : "JS book"
+
+}
+
+var message = "Book: "+ book;  //// you are adding string to an object..this book will be tried to be converted to a string///its toString will be called -- that toString is inherited from Object.prototype and returns the default value of [object Object]
+
+
+///if you are happy with the answer leave it,, sometimes it is usefule to define your own toString() method so that string conversions return a value that gives more information
+
+console.log(message);   //Book: [object Object]
+
+
+////lets do that shit
+
+var book1 = {
+	title :"php book",
+	toString : function (){
+		return this.title;
+	}
+}
+
+var message = "book : "+ book1;
+
+console.log(message);   ///book : php book  see you got correct shit now 
+
+
