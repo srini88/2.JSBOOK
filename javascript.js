5aa482ba34824951e475e9312ce248832e017244
srini88 <pvsrinivas.ece@gmail.com>
2016-04-14 21:39:26 -0500

    shit more

--- javascript.js
+++ javascript.js
@@ -4,6 +4,9 @@
 // A longer answer: If you declared your variable x with the var keyword at the time of first use, then delete x will not have any effect: x will still be available and hold the same value as before. As a partial workaround, you can set your variable to null or to undefined; however, this is not equivalent to deleting the variable, as shown in the example below.
 
 
+///setting to null makes the garbage collector  clean it
+
+
 b = 5;
 
 
