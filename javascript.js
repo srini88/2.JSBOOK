49481be1c615f2d47b54154506dfc39e20e01b14
srini88 <pvsrinivas.ece@gmail.com>
2016-04-20 00:22:57 -0500

    more shit

--- javascript.js
+++ javascript.js
@@ -1,20 +1,29 @@
-//you can also create objects with a null [[Prototype]] via Object.creat() such as..
+////2nd beast...
 
-var nakedObject = Object.create(null);
+////Constructor Inheritance....
 
-console.log("toString" in nakedObject);  //false  //no shit exists
-console.log("valueOf" in nakedObject); //false
+///////any function has a prototype property that can be modified or replaced...
 
-var obj = {}
+////That prototype property is automatically assigned to be a new generic object that inherits from Object.prototype and has a single own property called constructor........
 
 
-console.log("toString" in obj); //true //access to Object.prototype
-console.log("valueOf" in obj); //true
+function yourConstructor(){
 
-//this nakedObject in this example is an object with no prototype chain..
 
-///buit in methods wont be function...
+}
 
-///fucking blank slate..ghajini type...
+///in the back JS engine does this shit...
+////sets up construcor property for your shit....
 
-/////see you can create a protype-less object...
+yourConstructor.prototype = Object.create(Object.prototype, {
+
+	constructor : {
+		enumerable :true, 
+		configurable : true, 
+		writable : true, 
+		value : yourConstructor 
+	}
+});
+
+
+/////settting the constructor prototype propperty to an object that inherits from Object.prootytpe which means any instances of yourConstructor also inherit from object.prototype.....yourconstructor is a subtype of Object...and Object is a supertype of Yourconstructor...
