2e7f7704f1749e5630ad68f06022d5c9c0505b77
srini88 <pvsrinivas.ece@gmail.com>
2016-04-20 10:39:10 -0500

    bear 2

--- javascript.js
+++ javascript.js
@@ -11,11 +11,25 @@ Bear.prototype.growl = function(){
 	console.log(this.type + " says grr");
 }
 
-var grizzly = new Bear("grizzly");
+function Grizzly(){
+//so add this
+
+Bear.call(this, "grizzly");
+
+}
+
+//Grizzly.prototype = Bear.prototype  ///this is wrong...this is making it equal bear...we need a new object that is based off of prototype
+
+
+Grizzly.prototype = Object.create(Bear.prototype);  //everythign we have on Bear.prototype , we gonna apply to Grizzly proottpe
+
+
+var grizzly = new Grizzly;
 var polar = new Bear("polar");
-console.log(grizzly.growl(), polar.growl());  //
+console.log(grizzly.growl());  ///this is the problem ...here it prints undefined says grr  ---- why? 
+/////in Grizzly we are not callign ght econstructor of the Bear type..
 
-
+///now it says Grizzly says Grr 
 
 
 
