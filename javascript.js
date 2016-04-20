fb6db46c48752007ab3455b801be0be290116e83
srini88 <pvsrinivas.ece@gmail.com>
2016-04-20 00:07:12 -0500

    naked Shit

--- javascript.js
+++ javascript.js
@@ -1,60 +1,20 @@
-///inhertance between two different objects...
+//you can also create objects with a null [[Prototype]] via Object.creat() such as..
+
+var nakedObject = Object.create(null);
+
+console.log("toString" in nakedObject);  //false  //no shit exists
+console.log("valueOf" in nakedObject); //false
+
+var obj = {}
 
 
-var person1 = {
-name : "srini",
-sayName : function(){
-	console.log(this.name);
-}
-};
+console.log("toString" in obj); //true //access to Object.prototype
+console.log("valueOf" in obj); //true
 
-///  right side is big one... person1 is the prototype of person2 
-var person2 = Object.create(person1, {
+//this nakedObject in this example is an object with no prototype chain..
 
-	name :{
-		value : "Vas", 
-		configurable : true,
-		enumerable : true, 
-		writable : true
-	}
-});
+///buit in methods wont be function...
 
+///fucking blank slate..ghajini type...
 
-person1.sayName(); //srini
-person2.sayName();  //vas  - see person2 does not have sayName it got inhertited from person1 ...
-
-console.log(person1.hasOwnProperty("sayName"));  //true
-console.log(person1.isPrototypeOf(person2));   ///true ////left one is big..inside is small
-
-console.log(person2.isPrototypeOf(person1));  //false/ ///not reverse like this
-
-console.log(person2.hasOwnProperty("sayName"));   //false///inherited from right...does not contain it...
-
-
-var prop;
-for (prop in person2){
-	console.log("prop is: " + prop  + "  value is: " + person2[prop]);
-}
-
-//for in prints both own properties and prototype properties...you got sayName below...
-
-// prop is: name  value is: Vas
-// javascript.js:36 prop is: sayName  value is: function (){
-// 	console.log(this.name);
-// }
-
-var arr = Object.getOwnPropertyNames(person2);  //only fetches own properties
-console.log(arr);  //got just ["name"] here...
-
-
-////remmember this shit....
-
-console.log(Object.prototype.__proto__ === null);   /////true //always Object.prototype ka proto points to nullllll
-
-console.log(person2.__proto__);   ////points to person1 ///Object {name: "srini"}
-
-
-console.log(person2.__proto__ === person1);   //true 
-
-// That chain usually ends with
-// Object.prototype, whose [[Prototype]] is set to null.
+/////see you can create a protype-less object...
