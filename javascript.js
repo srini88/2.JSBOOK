5ca0e42187875b7ce38710a3b162869c81e57189
srini88 <pvsrinivas.ece@gmail.com>
2016-04-25 15:02:36 -0500

    closures 4

--- javascript.js
+++ javascript.js
@@ -1,41 +1,20 @@
+var addTo = function(passed){
 
-////closures....
+	var add = function(inner){
+		return passed + inner;
+	};
 
-//functions are also closures...in js...
-///function
-
-var me = "patrick bateman";
-
-function greetMe(){
-	console.log("hello " + me + "!");
-}
-
-greetMe();  //hello patrick bateman!  //this has access to outer variable scope..
-//greetMe will still remember...
-
-// we can access "me" because JS fns are closures...we cannot do this in langulages that does not support closures.....instead we have to pass an argument in other languages..
-
-////one use case of closures...
-
-// Your first example wasn't really a closure. It was just normal JavaScript scoping. Closures, on the other hand, are inner function that have access to variables of their outer functions even when those outer functions have been unwound from the stack. That is why functions that return functions are so powerful.﻿
-
-// Javascript Closure tutorial ( Closures Explained )
-// ///techsith
-
-//2/putting passed outside...
-///this is a godammn closure as well....
-///variables defined outside the fn are automatically available inside...
-////as JS uses lexical scoping... it does it by means of a closure..
-////any function where you using a variable from outside the scope are closures...
-
-var passed = 3;
-var addTo = function (){
-	var inner = 2;
-	return passed + inner;
+	return add;   ///////our addTo fn returns the function add
 };
-console.log(addTo());  //returns 5
 
-passed = 5;
+//console.dir(addTo(3));   //function add{}  ///returns add fn..with access to passed....which is being presereved...
 
-console.log(addTo());  //returns 7
+
+var addFour = addTo(3);   ///you are preserving this 3 value in all the subsequent function calls
+
+console.log(addFour(4)); //returns 7
+
+console.log(addTo(3)(4));  //this also returns 7
+
+///closures are functions with preserved data....
 
