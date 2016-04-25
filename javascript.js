87ca213c5e6e36363931eb6b29b5b1e7c18da31a
srini88 <pvsrinivas.ece@gmail.com>
2016-04-25 13:21:00 -0500

    reduce awesome

--- javascript.js
+++ javascript.js
@@ -7,30 +7,75 @@
 
 
 
-var orders = [
-	{amount : 1},
-	{amount : 2},
-	{amount : 3},
-	{amount : 4}
-];
+var orders = [1, 2,3,4];
+////very very importnat....
 
-///reduce wants an object unlike map and filter, 2nd argument..
-///this object as a starting point 
-var totalAmount = orders.reduce(function(sum, order){
-	console.log("accumulator: " +sum + " amount:  "+order.amount);
-	return sum+order.amount;
-}, 0);   /////passing accumulator as zero ...which will be sum in the first argument..
+// The first time the callback is called, previousValue and currentValue can be one of two values. If initialValue is provided in the call to reduce, then previousValue will be equal to initialValue and currentValue will be equal to the first value in the array. If no initialValue was provided, then previousValue will be equal to the first value in the array and currentValue will be equal to the second.
+
+
+//not giving initial value...
+
+
+// The value returned by reduce would be that of the last callback invocation (10).
+
+var totalAmount = orders.reduce(function(prev, current){
+	console.log("previous: " +prev + " current:  "+current);
+	return prev + current;
+}); 
 
 console.log(totalAmount);
-// accumulator: 0 amount:  1
-// accumulator: 1 amount:  2
-// accumulator: 3 amount:  3
-// accumulator: 6 amount:  4
-// 10
 
-///when I remove zero ..I'm getting..weird
+///now lets give initial value....
 
-// accumulator: [object Object] amount:  2
-// accumulator: [object Object]2 amount:  3
-// accumulator: [object Object]23 amount:  4
-// [object Object]234
+var totalAmount1 = orders.reduce(function(prev, current){
+	console.log("previous: " +prev + " current:  "+current);
+	return prev + current;
+}, 5); 
+
+console.log(totalAmount1);
+
+
+// previous: 5 current:  1
+// previous: 6 current:  2
+// previous: 8 current:  3
+// previous: 11 current:  4
+// 15
+
+var total = [0, 1, 2, 3].reduce(function(a, b) {
+	console.log (a, b);
+  return a + b;
+});
+
+console.log(total);   //0+1  //1+2  ///3 + 3    //answer 6
+
+
+//flatten a godammn array...
+var flattened = [[0, 1], [2, 3], [4, 5]].reduce(function(a, b) {
+
+	console.log(a, b);
+  return a.concat(b);
+}, []);
+// flattened is [0, 1, 2, 3, 4, 5]
+
+
+console.log(flattened);
+
+///as expected. - we passed an initialvalue of [] so a for first iteration will be []
+// [] [0, 1]
+// [0, 1] [2, 3]
+// [0, 1, 2, 3] [4, 5]
+// [0, 1, 2, 3, 4, 5]
+
+console.log("boo");
+//flatten a godammn array...
+var flattened1 = [[0, 1], [2, 3], [4, 5]].reduce(function(a, b) {
+
+	console.log(a, b);
+  return a.concat(b);
+});
+
+console.log(flattened1);  
+///as expected.... one iteration will be less...
+// [0, 1] [2, 3]
+// [0, 1, 2, 3] [4, 5]
+// [0, 1, 2, 3, 4, 5]
