94c4528da0fcf7021369dd705208cb549f1c9190
srini88 <pvsrinivas.ece@gmail.com>
2016-04-25 13:11:02 -0500

    reduce

--- javascript.js
+++ javascript.js
@@ -8,17 +8,29 @@
 
 
 var orders = [
-	{amount : 250},
-	{amount : 150},
-	{amount : 250},
-	{amount : 550}
+	{amount : 1},
+	{amount : 2},
+	{amount : 3},
+	{amount : 4}
 ];
 
-//traditional way...
+///reduce wants an object unlike map and filter, 2nd argument..
+///this object as a starting point 
+var totalAmount = orders.reduce(function(sum, order){
+	console.log("accumulator: " +sum + " amount:  "+order.amount);
+	return sum+order.amount;
+}, 0);   /////passing accumulator as zero ...which will be sum in the first argument..
 
-var totalAmount = 0;
-for (var i=0 ; i <orders.length ; i++){
-	totalAmount += orders[i].amount;
-}
+console.log(totalAmount);
+// accumulator: 0 amount:  1
+// accumulator: 1 amount:  2
+// accumulator: 3 amount:  3
+// accumulator: 6 amount:  4
+// 10
 
-console.log(totalAmount);  //1200
+///when I remove zero ..I'm getting..weird
+
+// accumulator: [object Object] amount:  2
+// accumulator: [object Object]2 amount:  3
+// accumulator: [object Object]23 amount:  4
+// [object Object]234
