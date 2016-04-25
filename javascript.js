07d9a29756e942bffa1b911278c6fc50b176fa49
srini88 <pvsrinivas.ece@gmail.com>
2016-04-25 14:47:21 -0500

    closures 1

--- javascript.js
+++ javascript.js
@@ -1,81 +1,35 @@
-////looking at reduce...
 
-////another higher order function reduce...
+////closures....
 
-///what is reduce?
-///reduce is the multitool of transformations...
+//functions are also closures...in js...
+///function
 
+var me = "patrick bateman";
 
+function greetMe(){
+	console.log("hello " + me + "!");
+}
 
-var orders = [1, 2,3,4];
-////very very importnat....
+greetMe();  //hello patrick bateman!  //this has access to outer variable scope..
+//greetMe will still remember...
 
-// The first time the callback is called, previousValue and currentValue can be one of two values. If initialValue is provided in the call to reduce, then previousValue will be equal to initialValue and currentValue will be equal to the first value in the array. If no initialValue was provided, then previousValue will be equal to the first value in the array and currentValue will be equal to the second.
+// we can access "me" because JS fns are closures...we cannot do this in langulages that does not support closures.....instead we have to pass an argument in other languages..
 
+////one use case of closures...
 
-//not giving initial value...
+// Your first example wasn't really a closure. It was just normal JavaScript scoping. Closures, on the other hand, are inner function that have access to variables of their outer functions even when those outer functions have been unwound from the stack. That is why functions that return functions are so powerful.﻿
 
-
-// The value returned by reduce would be that of the last callback invocation (10).
-
-var totalAmount = orders.reduce(function(prev, current){
-	console.log("previous: " +prev + " current:  "+current);
-	return prev + current;
-}); 
-
-console.log(totalAmount);
-
-///now lets give initial value....
-
-var totalAmount1 = orders.reduce(function(prev, current){
-	console.log("previous: " +prev + " current:  "+current);
-	return prev + current;
-}, 5); 
-
-console.log(totalAmount1);
+// Javascript Closure tutorial ( Closures Explained )
+// ///techsith
 
 
-// previous: 5 current:  1
-// previous: 6 current:  2
-// previous: 8 current:  3
-// previous: 11 current:  4
-// 15
 
-var total = [0, 1, 2, 3].reduce(function(a, b) {
-	console.log (a, b);
-  return a + b;
-});
+var addTo = function (passed){
+	var inner = 2;
+	return passed + inner;
+};
 
-console.log(total);   //0+1  //1+2  ///3 + 3    //answer 6
+console.log(addTo(3));  //5
 
 
-//flatten a godammn array...
-var flattened = [[0, 1], [2, 3], [4, 5]].reduce(function(a, b) {
-
-	console.log(a, b);
-  return a.concat(b);
-}, []);
-// flattened is [0, 1, 2, 3, 4, 5]
-
-
-console.log(flattened);
-
-///as expected. - we passed an initialvalue of [] so a for first iteration will be []
-// [] [0, 1]
-// [0, 1] [2, 3]
-// [0, 1, 2, 3] [4, 5]
-// [0, 1, 2, 3, 4, 5]
-
-console.log("boo");
-//flatten a godammn array...
-var flattened1 = [[0, 1], [2, 3], [4, 5]].reduce(function(a, b) {
-
-	console.log(a, b);
-  return a.concat(b);
-});
-
-console.log(flattened1);  
-///as expected.... one iteration will be less...
-// [0, 1] [2, 3]
-// [0, 1, 2, 3] [4, 5]
-// [0, 1, 2, 3, 4, 5]
+///putting passed outside...
