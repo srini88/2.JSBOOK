bc93f05a084a015dd197e3c16ed7a0013666698f
srini88 <pvsrinivas.ece@gmail.com>
2016-04-26 11:11:16 -0500

    more..snakify and hioppy

--- javascript.js
+++ javascript.js
@@ -1,29 +1,60 @@
 
-// http://www.sitepoint.com/higher-order-functions-javascript/
+//returning functions as results...
 
-///first class functions....
+//js allows to return other functions as a result...since fns are simply objects...they can be returned as sma eas any other value...
 
-// js treats functionsa as first claszs citizens...means objects...they can be assigned as the value of a variable...can be passed and returned just like any other refgerence variable...
-
-// fns use a callback...a cb is a fn that gets executed at the edn of the operation, once all all the other operations have been completed....
-
-
-// JS is single threaded...only one operation at a time.....each operation that is going to happen is queued along this single thread....we are allowing async behavior....
+////returning fns to create functions can be used as templates to create new fnuctions....
 
 
 
-var btn = document.getElementById("btn");
 
-// var elem = document.getElementById("para1");
-// console.log(elem);
-console.log(btn);
-btn.addEventListener("click", eve );
-
-//note we passed eve instead of eve() to our addEventListeneer function...when you pass a function by name without parantheses , tyou are passing the function object itself....when you pass it with parantehese, you are passing the result of executing that functin
+// imagine you’ve gotten tired of reading all of these articles about the specialness of Millennials, and you decide you want to replace the word Millennials with the phrase Snake People
 
 
-function eve(event){
 
-	console.log(event);
-	console.log("nigga you clicked the btn");
+
+var snakify = function(text){
+
+	return text.replace(/millenials/ig, "Snake People");
 }
+
+console.log(snakify("The Millenials are always upto something")); //The Snake People are always upto something
+
+
+///this works because it is pretty specific to one situation....
+//////like this you will have to crearte multiple shit for other use cases...
+
+////lets use currying shit...
+
+
+var attitude = function(original, replacement, source){
+
+	return function(source){
+		console.log(original, replacement, source);
+		return source.replace(original, replacement);
+	}
+}
+
+var snakify = attitude(/millenials/ig, "snake people");
+//snakify();   ///millenials/gi "snake people" undefined
+
+//console.log(snakify);  //returns a function //where you will have to pass in the source..
+
+var hippify = attitude(/baby boomers/ig, "Aging Hippies");
+
+console.log(snakify("The Millenials are always up to something.")); //// The Snake People are always up to something.
+
+console.log(hippify("The Baby Boomers just look the other way."));
+// The Aging Hippies just look the other way.
+
+
+//here we have isolated the code to actual work into a versatile ans extensible attitude functon , see both are attitude....that encapsulates all of the work needed to modofy any ionput string using an original phrase and a replacement pharese with some attitude...
+
+// Defining a new function as a reference to the attitude function, pre-populated with the first two arguments it takes, allows the new function to take whatever argument you pass it and use that as the source text in the internal function returned by the attutide function.
+
+
+// We’re taking advantage here of the fact that JavaScript functions don’t care whether they are passed the same number of arguments as they were originally defined to take. If an argument is missing, the function will just treat the missing arguments as undefined.
+
+// On the other hand, that additional argument can be passed in later when the function being called has been defined in the way we just demonstrated, as a reference to a function returned from another function with an argument (or more) left undefined.
+
+// We’re creating a template function that returns another function. Then we’re defining that newly returned function, minus one attribute, as a custom implementation of the template function. All the functions created this way will inherit the same working code from the template function, but can be predefined with different default arguments.
