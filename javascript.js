2916aac83e7ef885f9fb1ca185b2ee599024c122
srini88 <pvsrinivas.ece@gmail.com>
2016-05-02 22:03:13 -0500

    Singleton Pattern starting..no return instance

--- javascript.js
+++ javascript.js
@@ -1,40 +1,39 @@
+////singleton pattern
 
+///Singleton pattern.....
+//IIFE
+var NameSpace={};
+NameSpace.Singleton = (function(){
 
-///creating a range function in javascript....
+	//private members...	
+	var privateAttr1 = false;
+	var privateAttr2 = [1, 2, 3];
 
-/**
- * Creates a range of numbers in an array, starting at a specified number and
- * ending before a different specified number.
- * @param {number} start  Indicates what number should be used as the first
- *     number in the returned array.  If this is the only number argument
- *     supplied, this will be used as the edge and 0 will be used as the start.
- * @param {number=} edge  Indicates the first number that should not appear in
- *     the range of numbers.  If this number preceeds the start in the range
- *     (taking into account the step), an empty array will be returned.  If not
- *     specified and not inferred this defaults to 0.
- * @param {number=} step  Indicates the difference between one number and the
- *     subsequent number placed in the returned array.  If not specified this
- *     defaults to 1.
- * @return {!Array.<number>}  Array of numbers in the specified range.
- */
+	function privateMethod1(){
 
-
-
-function range(start, edge, stop){
-	console.log(arguments.length);  //1
-	//if only one argument was passed, make it the edge and 0 the start..
-	if (arguments.length == 1){
-		edge = start;   ///first arg is in start..so we nneed tht for edge
-		start =0;
 	}
+	function privateMethod2(){
 
-	step =1;
-
-	//create an array of numbers
-	for (var ret = []; (edge-start) * step>0; start +=step){
-		ret.push(start);
 	}
-	return ret;
-}
-console.log(range(10));   //prints [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+	return { //public members..
+		publicAttr1 : true, 
+		publicAttr2 : 21,
+		publicMethod1 :function(){
 
+		},
+		publicMethod2 : function(){
+
+		}
+	};
+})();
+
+console.log(NameSpace);
+
+// What are the advantages of this pattern?
+
+// Namespacing
+// Grouping related methods and attributes together.
+// What are the drawbacks of this pattern?
+
+// Tightly coupled
+// Unit testing is difficult
