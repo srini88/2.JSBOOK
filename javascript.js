2afc19323ae7c147b9e22cc751bc321c7794f556
srini88 <pvsrinivas.ece@gmail.com>
2016-05-23 22:51:46 -0500

    shallowCopy only

--- javascript.js
+++ javascript.js
@@ -1,34 +1,35 @@
-// Shallow copy vs deep copy
+////JUST SHALLOW COPY>..
 
-// A shallow copy will clone the top-level object, but nested object are shared between the original and the clone. That's it: an object reference is copied, not cloned. So if the original object contains nested object, then the clone will not be a distinct entity.
 
-// A deep copy will recursively clone every objects it encounters. The clone and the original object will not share anything, so the clone will be a fully distinct entity.
+// With jQuery, you can shallow copy with:
 
-// Shallow copies are faster than deep copies.
+// var copiedObject = jQuery.extend({}, originalObject)
+// subsequent changes to the copiedObject will not affect the originalObject, and vice versa. - very imp
 
-// When it is ok to share some data, you may use shallow copy. There are even use case where it is the best way to do the job. But whenever you need to clone a deep and complex data structure, a tree, you will have to perform deep copy. Have in mind that on really big tree, it can be an expensive operation.
 
 
 var obj = {
 	a: 1,
 	b:2
 }
-// function shallowCopy(oldObj) {
-//     var newObj = {};
-//     for(var i in oldObj) {
-//         if(oldObj.hasOwnProperty(i)) {
-//             newObj[i] = oldObj[i];
-//         }
-//     }
-//     return newObj;
-// }
+function shallowCopy(oldObj) {
+    var newObj = {};
+    for(var i in oldObj) {
+        if(oldObj.hasOwnProperty(i)) {
+            newObj[i] = oldObj[i];
+        }
+    }
+    return newObj;
+}
 
-// var newObj = shallowCopy(obj);
+var newObj = shallowCopy(obj);
 
-var newObj = obj   //this is shit..changin newObj changes obj..not even a shallow copy..
+
 
 newObj.a=2;
-console.log(newObj)
+console.log(newObj)  /// a:2, b:2
+console.log(obj)  //still old shit//// a:1, b:2  ///one more level deep..it will be a problem...obj and newObj point to the same shit..
 
-console.log(obj)
 
+// Shallow copy keeps references to original objects, arrays or functions within the new object, so the "copy" is still linked to the original object. In other words, they will be pointing to the same memory location.
+var newObj = $.extend({}, oldObj);
