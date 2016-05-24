5e723abe80f464dbfb317c99d00b0b3c0ded9832
srini88 <pvsrinivas.ece@gmail.com>
2016-05-24 16:37:05 -0500

    more Obj.create

--- javascript.js
+++ javascript.js
@@ -1,31 +1,11 @@
-
-var Person = function(){
-
-	this.name = "srini";
-	this.age = 21;
-}
-
-console.log(Person.prototype)  //empty object..no shit added
-
-//problem with this here
-Person.prototype = {
-
-	sayName : function(){
-		console.log("Saying name");
-	},
-	tellMore :function(){
-		console.log("telling more");
+var book = Object.create(Object.prototype, {
+	title :{
+		value :"JS",
+		writable:true,
+		enumerable :true,
+		configurable :true
 	}
+})
 
-}
-
-
-console.log(Person.prototype)  //above 2 methods will be there
-
-var p1 = new Person;
-
-
-console.log(p1.constructor)  //going to Object...main obj
-console.log(p1.constructor === Person)  //getting false, because we fucked up prototype 
-
-console.log(p1.constructor === Object)  //true..went to main obj
+console.log(book)
+console.log(Object.prototype.isPrototypeOf(book))  //true
