2d9c8745d737072234edf95a779167ceba560334
srini88 <pvsrinivas.ece@gmail.com>
2016-05-23 22:06:58 -0500

    rev closures iffe

--- javascript.js
+++ javascript.js
@@ -1,16 +1,13 @@
-
-///closures trying out...
-///if I dont use IIFE
-var ab = function(){
+//or else do this
+//using IIFE now
+var ab = (function(){
 
 	var counter =0;
 	return function(){
 		return ++counter;
 	}
-}
+})();
 
-var counter =ab();  //got to use this to execute this shit..
-
-console.log(counter());  //1
-console.log(counter());  //2
+console.log(ab());  //1
+console.log(ab());  //2
 
