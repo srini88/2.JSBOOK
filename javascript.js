bde1a04249be8de4ba187a9de7593915e6809514
srini88 <pvsrinivas.ece@gmail.com>
2016-05-23 22:05:10 -0500

    something notsure

--- javascript.js
+++ javascript.js
@@ -1,73 +1,29 @@
 
-///// in the previous post, we saw the basic structure of a JS singleton pattern..
+the problem with object oriented code...
 
-// what is the need of JS lazy loading singleton pattern? 
+objects separate code and prevent spaghetti ..
 
-// You can understand that JavaScript Singleton pattern which explain in previous post becomes performance issue if the code inside singleton object becomes more and more because the ananimous function executes immediately and returns properties, function, etc into singleton object immediately when .js files loads.
+but the parts of a pgm Need to know about one -another..
 
-// So it is wise to execute the function when it requires really. For that we need to define getInstance() method which checks to see whether the singleton has been instantiated. If not, it will instantiated and returned.  If it has, a stored copy is returned.
+which leads back towards spaghetti ..
 
-
-var NameSpace={};
-NameSpace.Singleton = (function(){
-
-	var instance;
+the problem is dependency...
 
 
-	function init(){   //function init is abstracted..
-	//private members...	
-	/////console.log("boo")    //this does not get printed...
-	var privateAttr1 = false;
-	var privateAttr2 = [1, 2, 3];
 
-	function privateMethod1(){
+singleton and static methods...
 
-	}
-	function privateMethod2(){
-
-	}
-	return { //public members..   //this return returns to init
-		publicAttr1 : true, 
-		publicAttr2 : 21,
-		publicMethod1 :function(){
-			return "fuck you nigga " + privateAttr1;  //this.privateAttr1 gives undefined
-		},
-		publicMethod2 : function(){
-			return "you are a sucker";
-		}
-	};
-} //close of Init
-	return {   //public interface...
-		getInstance: function(){
-
-			if (!instance){  //check already exists
-				console.log("creating instance");
-				instance = init();
-				return instance;
-			}
-			else{
-				console.log("returning 2nd hand");
-				return instance;
-			}
-			
-		}
+A singleton is a class --- only one object will exist during the life time of application..
 
 
-	}
+how do I get access to instance from a random location -- advantage...
 
-})();
 
-console.log(NameSpace);
-//now all clean...you can see it does not print creating instance by default...this is the lazy load shit...... it will only create wihen you ask for it...
-//previously it creates and put it in memory....
+Anytime I communicate or access with the PlayerModel I m making a concrete dependency somewhere in the app...
 
-///now adding more...
+makes the application interconnected...
 
-console.log(NameSpace.Singleton.getInstance().publicMethod1());
 
-console.log(NameSpace.Singleton.getInstance().publicMethod2());
-
-// creating instance
-// fuck you nigga false
-// returning 2nd hand
-// you are a sucker
+--- code rapidly becomes interdependent...
+= highly focused on all classes working correctly...
+- 
