cc1a409cbc3a3d52219fb46688049d01a9bb024f
srini88 <pvsrinivas.ece@gmail.com>
2016-05-23 22:53:46 -0500

    JS deep copy

--- javascript.js
+++ javascript.js
@@ -1,35 +1,16 @@
-////JUST SHALLOW COPY>..
+////now dealing with deep copy..
 
+var newObj = $.extend(true, {}, oldObj);
 
-// With jQuery, you can shallow copy with:
-
-// var copiedObject = jQuery.extend({}, originalObject)
-// subsequent changes to the copiedObject will not affect the originalObject, and vice versa. - very imp
-
-
-
-var obj = {
-	a: 1,
-	b:2
-}
-function shallowCopy(oldObj) {
-    var newObj = {};
-    for(var i in oldObj) {
-        if(oldObj.hasOwnProperty(i)) {
-            newObj[i] = oldObj[i];
+function deepCopy(oldObj) {
+    var newObj = oldObj;
+    if (oldObj && typeof oldObj === 'object') {
+        newObj = Object.prototype.toString.call(oldObj) === "[object Array]" ? [] : {};
+        for (var i in oldObj) {
+            newObj[i] = deepCopy(oldObj[i]);
         }
     }
     return newObj;
 }
 
-var newObj = shallowCopy(obj);
-
-
-
-newObj.a=2;
-console.log(newObj)  /// a:2, b:2
-console.log(obj)  //still old shit//// a:1, b:2  ///one more level deep..it will be a problem...obj and newObj point to the same shit..
-
-
-// Shallow copy keeps references to original objects, arrays or functions within the new object, so the "copy" is still linked to the original object. In other words, they will be pointing to the same memory location.
-var newObj = $.extend({}, oldObj);
+In jQuery, $.clone() method only clones DOM elements.
