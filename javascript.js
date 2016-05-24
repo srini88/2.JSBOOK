8e468d21730e97520f09293c14fc3648520c4c3f
srini88 <pvsrinivas.ece@gmail.com>
2016-05-23 22:13:14 -0500

    sorting with slice

--- javascript.js
+++ javascript.js
@@ -1,13 +1,14 @@
-//or else do this
-//using IIFE now
-var ab = (function(){
 
-	var counter =0;
-	return function(){
-		return ++counter;
-	}
-})();
+//sorting numbers...without argunents
+function add(){
+	var arr  = Array.prototype.slice.call(arguments); //slice takes start and end...herepassing only start
+	console.log(arr);  //this makes it array..
 
-console.log(ab());  //1
-console.log(ab());  //2
+	arr.sort(); 
+	console.log(arguments)  //this is an object with some extra shit..
+	//arguments.sort();
+	//return arguments;
+	return arr;   //this code works.
+}
 
+console.log(add(9, 5, 7, 6));
