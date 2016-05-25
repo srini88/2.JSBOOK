ebec8551e83a34908050ff13118c45451143d306
srini88 <pvsrinivas.ece@gmail.com>
2016-05-25 14:51:40 -0500

    more

--- javascript.js
+++ javascript.js
@@ -1,28 +1,23 @@
-
-///Learning common patterns with JS prototypes....
-function Bear(){
-    if (!(this instanceof Bear)) return new Bear();
-}
- var grizzly = Bear();
- console.log(grizzly)
-
-///doing inheritance with objects literal notation..
-
-var person1  = {
-    name :"srini"
+function Drink(drink){
+    this.drink = drink;
 
 }
 
+Drink.prototype.getDrink = function(){
+    return this.drink;
+}
 
-var person2 = Object.create(person1);
 
-console.log(person2.name)  //got this stuff from person1 through inheritance..
+var coke = new Drink("coke");
 
-console.log(person1.isPrototypeOf(person2)) //true
+console.log(coke.constructor === Drink)  //true
 
-person2proto = Object.getPrototypeOf(person2);
+console.log(Drink.prototype.isPrototypeOf(coke))  ////true
 
-console.log(person2proto)  ///person1 object itself...
 
-console.log(person2proto === person1)  //true
-console.log(person2.constructor)  //shows function Object...//constructor stuff not working correctly since you hhave not used constructors here...
+console.log(Drink.prototype.isPrototypeOf(new Drink)) //true
+
+
+
+console.log(Drink.constructor)  //some native shit
+
