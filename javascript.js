442adc8d729977262d36ebda39741cdf2980c701
srini88 <pvsrinivas.ece@gmail.com>
2016-05-25 10:14:38 -0500

    more

--- javascript.js
+++ javascript.js
@@ -1,26 +1,19 @@
-//var str = "aabcccccaaac";
-var str = "abcd";
-
-function manipulation (str){
-
-	var output = "";
-	var temp  = str[0];
-	var count = 1;
-	for (var i=1; i <str.length; ++i){
-
-		 if(str[i] === temp){
-		 	++count;
-		 }
-		 else{
-		 	output += temp+count;
-
-		 	temp = str[i]; ///
-		 	count =1;   //came to new character..count updating to 1
-		 }
-	}
-	output += temp+count;  //without this getting a2b1c5
-	return (output.length > str.length)? str: output;
+function validParentheses(n) {
+    var combos = [ ];
+    function getCombinations(openNum,closingNum,curr) {
+        if (openNum === 0 && closingNum === 0)
+            combos.push(curr);
+        if (openNum > 0) {
+            getCombinations(openNum-1, closingNum + 1, curr + "(");
+        }
+        if (closingNum > 0) {
+            getCombinations(openNum, closingNum - 1, curr + ")");
+        }
+    }
+    getCombinations(n,0,"");
+    return combos.length;
 
 }
 
-console.log(manipulation(str));  //a2b1c5
+
+console.log(validParentheses(4))
