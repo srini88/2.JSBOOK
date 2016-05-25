f1619ca2e87e683d19e1abbe941b9d4a8d760775
srini88 <pvsrinivas.ece@gmail.com>
2016-05-25 11:41:46 -0500

    creating new instance

--- javascript.js
+++ javascript.js
@@ -1,33 +1,7 @@
 
-function Bear(type){
-    this.type = type;
+///Learning common patterns with JS prototypes....
+function Bear(){
+    if (!(this instanceof Bear)) return new Bear();
 }
-Bear.prototype.growl = function(){
-    console.log(this.type + " says grr");
-}
-///create another class called grizzly
-
-function Grizzly(name){
-    Bear.call(this,name);  //cll this Bear  constructor using this instance
-}
-
-
-
-Grizzly.prototype = Object.create(Bear.prototype);
-
-// Grizzly.prototype.growl = function(){
-//     console.log("on the grizzly protottype")
-// }
-var grizzly = new Grizzly('grizz'); 
- 
-
-console.log(grizzly.growl())   //prints on the grizzly protottype
-
-//it checks is there a growl function on grizzly, no ..then go to Grizzly's prototype found it rthere...
-
-
-///how does it know growl 
-///is there a grown fulction here in grizzly , or Grizzly.prototype or Bear.prototyp
-
-
-///THis works.......calling Bear.call in Grizzly.... and setting Grizzly protoyep to bear.prototype .....2 things you are setting....  i'm not sure where I saw three variabtions....think
+ var grizzly = Bear();
+ console.log(grizzly)
