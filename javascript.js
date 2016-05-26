81cce1c10aac5577a3407b3d14eeeeb317fcd5a5
srini88 <pvsrinivas.ece@gmail.com>
2016-05-26 12:43:39 -0500

    shallow copy and normal

--- javascript.js
+++ javascript.js
@@ -1,28 +1,38 @@
-var singleton = (function(){
-console.log("hi")
-	var instance ;
-	var p1;
-	var p2;
+var p1 ={
 
-	function init(){
-		return {
-			p1:5, 
-			p2 :6,
-			pMethod :function(){
-				return "fuck off";
-			}
-		}
-	}
-	return {
-		getInstance :function(){
-			if (!instance){
-				console.log("creating instance")
-				instance = init();
-			}
-			return instance;
-		}
-	}
-})();
+	a:3, 
+	b:4
+}
 
-console.log(singleton.getInstance().pMethod())
-console.log(singleton.getInstance().pMethod())
+//do a shallow copy..
+
+function sCopy(old){
+	var newObj ={};
+
+
+	var prop;
+	for (prop in old){  //doont want to add prototype shit...
+		///better to include another if with   if(oldObj.hasOwnProperty(i))
+		//console.log(prop, old[prop])
+		//console.log(newObj.prop) //this is wrong ..does not work..use bracket notatiin..
+		newObj[prop] = old[prop];
+
+
+	}
+
+	return newObj;
+
+}
+
+var newObj = sCopy(p1);
+console.log(newObj)
+
+newObj.a = 10;  //chaning new Obj does not change p1 ni the next line..shallowCopy
+console.log(newObj)
+console.log(p1)  //does not get changed
+
+var p2 = p1;  //this is shit.. not even a shallow copy..
+
+p2.a = 6;
+console.log(p2);  //changing p2 changes p1 in the next line
+console.log(p1)  //shit gets changed..
